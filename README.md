f2312438baebbe9369c265cfd6b08cb7ab5abd93
Newton Jain <newton.jain@gmail.com>
2017-11-13 15:59:36 -0500

    Added a Readme file

---
+++ README.md
@@ -0,0 +1,37 @@
+# Call Houndify
+
+This application allows you to make voice and text queries to the Houndify API from your phone. 
+
+
+## Features
+
+* Voice based queries
+* Text based search
+
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
+
+```sh
+git clone https://github.com/newtonjain/Call-Houndify.git # or clone your own fork
+cd Call-Houndify
+npm install
+npm start
+```
+
+Your app should now be running on [localhost:3000](http://localhost:3000/).
+
+
+## Documentation
+
+For more information about using Houndify, see these docs:
+
+- [Creating your First Houndify Client](https://medium.com/houndify/houndify-tutorial-create-client-try-api-c54103878e3b)
+- [Houndify Web SDK](https://docs.houndify.com/sdks/docs/web)
+- [Setting up Voice Request](https://docs.houndify.com/sdks/docs/web#voicerequest)
+- [Setting up text request](https://docs.houndify.com/sdks/docs/web#textrequest)
+
+### License
+
+Licensed under the MIT License
--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var bodyParser = require('body-parser');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
-
 //parse arguments
 var argv = require('minimist')(process.argv.slice(2));
 
@@ -19,189 +18,195 @@ var config = require(path.join(__dirname, configFile));
 
 //express app
 var app = express();
-var port = process.env.PORT|| config.port || 8446;
+var port = process.env.PORT || config.port || 8446;
 // var publicFolder = argv.public || 'public';
 // app.use(express.static(path.join(__dirname, publicFolder)));
 
-
-
 //Initialize HoundifyClient with Client ID, Client Key and callback methods.
 var myClient = new Houndify.HoundifyClient({
-  clientId:  config.clientId, 
-  clientKey: config.clientKey,
-  onResponse: function(response, info) {
-    console.log("HERE IS THE RESPONSE", response);
-    //get current conversation state for a user and save it somewhere
-    //you can then re-set it later, before sending another request for that user
-    var conversationState = myClient.conversation.getState();
-    myClient.conversation.setState(conversationState);
-    return response;
-  },
-  onError: function(err, info) {
-    console.log("HERE IS THE ERROR", err);
-  }
+    clientId: config.clientId,
+    clientKey: config.clientKey,
+    onResponse: function(response, info) {
+        console.log("HERE IS THE RESPONSE", response);
+        //get current conversation state for a user and save it somewhere
+        //you can then re-set it later, before sending another request for that user
+        var conversationState = myClient.conversation.getState();
+        myClient.conversation.setState(conversationState);
+        return response;
+    },
+    onError: function(err, info) {
+        console.log("HERE IS THE ERROR", err);
+    }
 });
 
 //REQUEST INFO JSON
 //see https://houndify.com/reference/RequestInfo
 var requestInfo = {
-  ClientID: config.clientId, 
-  UserID: "test_user",
-  Latitude: 37.388309, 
-  Longitude: -121.973968
+    ClientID: config.clientId,
+    UserID: "test_user",
+    Latitude: 37.388309,
+    Longitude: -121.973968
 }
 
 
 if (config.https) {
-  
     //ssl credentials
     var privateKey = fs.readFileSync(config.sslKeyFile);
     var certificate = fs.readFileSync(config.sslCrtFile);
-    var credentials = { key: privateKey, cert: certificate };
-  
+    var credentials = {
+        key: privateKey,
+        cert: certificate
+    };
+
     //https server
     var httpsServer = https.createServer(credentials, app);
     httpsServer.listen(port, function() {
-      console.log("HTTPS server running on port", port);
-      console.log("Open https://localhost:" + port, "in the browser to view the Web SDK demo");
+        console.log("HTTPS server running on port", port);
+        console.log("Open https://localhost:" + port, "in the browser to view the Web SDK demo");
     });
-  
-  } else {
-  
+
+} else {
+
     app.listen(port, function() {
-      console.log("HTTP server running on port", port);
-      console.log("Open http://localhost:" + port, "in the browser to view the Web SDK demo");
+        console.log("HTTP server running on port", port);
+        console.log("Open http://localhost:" + port, "in the browser to view the Web SDK demo");
     });
-  
-  }
 
-  
-  // const app = express();
-  
-  app.use(bodyParser());
+}
 
-  app.get('/', function(req, res) {
+
+// const app = express();
+
+app.use(bodyParser());
+
+app.get('/', function(req, res) {
     res.send("Hello World");
 })
-  
-  app.post('/sms', (req, res) => {
+
+app.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
     var query = req.body.Body;
     //Send the query from arguments with request info object
 
     var myClientText = new Houndify.HoundifyClient({
-      clientId:  config.clientId, 
-      clientKey: config.clientKey,
-      onResponse: function(response, info) {
-        console.log("HERE IS THE RESPONSE", response);
-        //res.send(response);
-        twiml.message(response.AllResults[0].SpokenResponse);
-        //get current conversation state for a user and save it somewhere
-        //you can then re-set it later, before sending another request for that user
-        var conversationState = myClient.conversation.getState();
-        myClient.conversation.setState(conversationState);
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        res.end(twiml.toString());
-      },
-      onError: function(err, info) {
-        console.log("HERE IS THE ERROR", err);
-      }
+        clientId: config.clientId,
+        clientKey: config.clientKey,
+        onResponse: function(response, info) {
+            console.log("HERE IS THE RESPONSE", response);
+            //res.send(response);
+            twiml.message(response.AllResults[0].SpokenResponse);
+            //get current conversation state for a user and save it somewhere
+            //you can then re-set it later, before sending another request for that user
+            var conversationState = myClient.conversation.getState();
+            myClient.conversation.setState(conversationState);
+            res.writeHead(200, {
+                'Content-Type': 'text/xml'
+            });
+            res.end(twiml.toString());
+        },
+        onError: function(err, info) {
+            console.log("HERE IS THE ERROR", err);
+        }
     });
 
     myClientText.textSearch.query(query, requestInfo);
 
-  });
-  
-  app.get('/search', function(req, res) {
+});
+
+app.get('/search', function(req, res) {
     var query = req.query.thebigquestion;
     //Send the query from arguments with request info object
 
     var myClientText = new Houndify.HoundifyClient({
-      clientId:  config.clientId, 
-      clientKey: config.clientKey,
-      onResponse: function(response, info) {
-        console.log("HERE IS THE RESPONSE", response);
-        res.send(response.AllResults[0].SpokenResponse);
-        //get current conversation state for a user and save it somewhere
-        //you can then re-set it later, before sending another request for that user
-        var conversationState = myClient.conversation.getState();
-        myClient.conversation.setState(conversationState);
-      },
-      onError: function(err, info) {
-        console.log("HERE IS THE ERROR", err);
-      }
+        clientId: config.clientId,
+        clientKey: config.clientKey,
+        onResponse: function(response, info) {
+            console.log("HERE IS THE RESPONSE", response);
+            res.send(response.AllResults[0].SpokenResponse);
+            //get current conversation state for a user and save it somewhere
+            //you can then re-set it later, before sending another request for that user
+            var conversationState = myClient.conversation.getState();
+            myClient.conversation.setState(conversationState);
+        },
+        onError: function(err, info) {
+            console.log("HERE IS THE ERROR", err);
+        }
     });
 
     myClientText.textSearch.query(query, requestInfo);
-  })
+})
 
-  //When Twilio posts the voice blob to the backend.
-  app.post('/voice', (request, response) => {
+//When Twilio posts the voice blob to the backend.
+app.post('/voice', (request, response) => {
     // Use the Twilio Node.js SDK to build an XML response
     const twiml = new VoiceResponse();
-    twiml.say({ voice: 'alice' }, 'hello world!');
-  
+    twiml.say({
+        voice: 'alice'
+    }, 'hello world!');
+
     // Render the response as XML in reply to the webhook request
     response.type('text/xml');
     response.send(twiml.toString());
-  });
+});
 
-  // Create a route that will handle Twilio webhook requests, sent as an
+// Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/voice2', (request, response) => {
-  // Get information about the incoming call, like the city associated
-  // with the phone number (if Twilio can discover it)
-  const city = request.body.FromCity || 'Toronto';
+    // Get information about the incoming call, like the city associated
+    // with the phone number (if Twilio can discover it)
+    const city = request.body.FromCity || 'Toronto';
 
-  // Use the Twilio Node.js SDK to build an XML response
-  const twiml = new VoiceResponse();
-  twiml.say({voice: 'alice'},
-    `Welcome to Call the Web powered by Houndify. How can I help you today?`
-  );
-  twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
+    // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+    twiml.say({
+            voice: 'alice'
+        },
+        `Welcome to Call the Web powered by Houndify. How can I help you today?`
+    );
+    twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
 
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
+    // Render the response as XML in reply to the webhook request
+    response.type('text/xml');
+    response.send(twiml.toString());
 });
 
 
-  //When user is loading a file from the local path. 
-  app.get('/file', function(req, res){
+//When user is loading a file from the local path. 
+app.get('/file', function(req, res) {
 
     var myClientVoice = new Houndify.HoundifyClient({
-      clientId:  config.clientId, 
-      clientKey: config.clientKey,
-      onResponse: function(response, info) {
-        console.log("HERE IS THE VOICE RESPONSE", response);
-        res.send(response);
+        clientId: config.clientId,
+        clientKey: config.clientKey,
+        onResponse: function(response, info) {
+            console.log("HERE IS THE VOICE RESPONSE", response);
+            res.send(response);
 
-        //get current conversation state for a user and save it somewhere
-        //you can then re-set it later, before sending another request for that user
-        var conversationState = myClient.conversation.getState();
-        myClient.conversation.setState(conversationState);
-      },
-      onError: function(err, info) {
-        console.log("HERE IS THE ERROR", err);
-      }
+            //get current conversation state for a user and save it somewhere
+            //you can then re-set it later, before sending another request for that user
+            var conversationState = myClient.conversation.getState();
+            myClient.conversation.setState(conversationState);
+        },
+        onError: function(err, info) {
+            console.log("HERE IS THE ERROR", err);
+        }
     });
 
 
     //Read test wave and stream it to Houndify backend
     var reader = new wav.Reader();
-    reader.on('format', function (format) {
-      console.log("HERE IS THE FORMAT", format);
-      myClientVoice.voiceSearch.startStreaming(requestInfo, 16000);
+    reader.on('format', function(format) {
+        console.log("HERE IS THE FORMAT", format);
+        myClientVoice.voiceSearch.startStreaming(requestInfo, 16000);
     });
 
-    reader.on('data', function (chunk) {
-      var arrayBuffer = new Uint8Array(chunk).buffer;
-      var view = new Int16Array(arrayBuffer);
-      myClientVoice.voiceSearch.write(view);
+    reader.on('data', function(chunk) {
+        var arrayBuffer = new Uint8Array(chunk).buffer;
+        var view = new Int16Array(arrayBuffer);
+        myClientVoice.voiceSearch.write(view);
     });
 
-    reader.on('end', function() { 
-      myClientVoice.voiceSearch.stop(); 
+    reader.on('end', function() {
+        myClientVoice.voiceSearch.stop();
     });
 
 
@@ -210,4 +215,4 @@ app.post('/voice2', (request, response) => {
     var file = fs.createReadStream(audioFilePath);
     file.pipe(reader);
 
-  })
+})
